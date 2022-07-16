05f7e7b0c8014b3edb10531eeb36100873d611f4
kjebali23 <khalil.jebali.kj@gmail.com>
2022-07-16 15:08:26 +0100

    Adding popup functionalities

--- contentScript.js
+++ contentScript.js
@@ -55,6 +55,23 @@
         })
     }
 
+
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, value, videoId } = obj;
+    
+        if (type === "NEW") {
+          currentVideo = videoId;
+          newVideoLoaded();
+        } else if (type === "PLAY") {
+          youtubePlayer.currentTime = value;
+        } else if ( type === "DELETE") {
+          currentVideoBookmarks = currentVideoBookmarks.filter((b) => b.time != value);
+          chrome.storage.sync.set({ [currentVideo]: JSON.stringify(currentVideoBookmarks) });
+    
+          response(currentVideoBookmarks);
+        }
+      });
+
     newVideoLoaded();
 })();
 
--- utils.js
+++ utils.js
@@ -1,5 +1,8 @@
-export async function getActiveTabUrl(){
-    let queryOptions = {active: true , currentWindow : true};
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+export async function getActiveTabURL() {
+    const tabs = await chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    });
+  
+    return tabs[0];
 }
